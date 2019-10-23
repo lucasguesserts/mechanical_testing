fa8954e37dbb280e95c45f17c22bcfbbad457039
Lucas Guesser Targino da Silva <lucasguesserts@gmail.com>
2019-10-23 12:39:40 -0300

    TensileTest constructor implementation started.
    Reading file implemented.

--- mechanical_testing/TensileTest.py
+++ mechanical_testing/TensileTest.py
@@ -1,2 +1,16 @@
+import numpy as np
+import pandas as pd
+import copy
+
 class TensileTest:
-    pass
+	def __init__(self, file):
+		self._readFromFile(file)
+		return
+
+	def _readFromFile(self, file):
+		df = pd.read_csv(filepath_or_buffer=file)
+		self.force        = copy.deepcopy(np.array(df['force']).flatten())
+		self.displacement = copy.deepcopy(np.array(df['displacement']).flatten())
+		self.time         = copy.deepcopy(np.array(df['time']).flatten())
+		del df
+		return
--- test/test_TensileTest.py
+++ test/test_TensileTest.py
@@ -1,5 +1,12 @@
+import pytest
+import numpy as np
 import mechanical_testing as mect
 
-def test_simple_test():
-	assert True
+def test_read_csv():
+	tensile = mect.TensileTest('./test/data/tensile/tensile_steel_1045.csv')
+	maxLocation = np.argmax(tensile.force)
+	assert maxLocation                       ==               416
+	assert tensile.time[maxLocation]         == pytest.approx(46.183,    rel=1E-12)
+	assert tensile.displacement[maxLocation] == pytest.approx(0.0013913, rel=1E-12)
+	assert tensile.force[maxLocation]        == pytest.approx(74715.3,   rel=1E-12)
 	return
